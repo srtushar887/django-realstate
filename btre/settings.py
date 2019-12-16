3b68a2495170716a57c054b248569c23a0efd619
srtushar887 <sr.tushar.pro@gmail.com>
2019-12-17 02:43:06 +0600

    complete

--- btre/settings.py
+++ btre/settings.py
@@ -34,12 +34,14 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
@@ -133,3 +135,12 @@ STATICFILES_DIRS = [
 # media folder setting
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+
+
+# messages
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+
+}
--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404,render
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 # Create your views here.
-def listings(request):
-    return render(request,'listings/listings.html')
 
-def listing(request):
-    return render(request,'listings/listing.html')
+from .models import Listing
+
+def listings(request):
+
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True);
+
+    paginator = Paginator(listings, 1)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    context = {
+        'listings' : paged_listings
+    }
+    return render(request,'listings/listings.html',context)
+
+def listing(request,listing_id):
+
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+        'listing' : listing
+    }
+    return render(request,'listings/listing.html',context)
 
 def search(request):
     return render(request,'listings/search.html')
